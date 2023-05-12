64e53c024b04f2cc6799f029ecd9ae38261f626c
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-05-12 12:13:38 +0545

    Initial Commit

---
+++ CoreBluetoothTutorial/CoreBluetoothTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothTutorialApp.swift
+//  CoreBluetoothTutorial
+//
+//  Created by Sabal on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
